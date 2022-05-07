2d213abb0d4ab17b5cb1dcefa9bf697c857c8bd8
trevor16gordon <trevor16gordon@gmail.com>
2022-05-07 16:22:24 -0400

    Adding deepmind control suite environment

--- src/env.py
+++ src/env.py
@@ -144,3 +144,78 @@ class GymEnv(BaseEnv):
     # Sample an action randomly from a uniform distribution over all valid actions
     def sample_random_action(self):
         return np.array(self._env.action_space.sample())
+
+
+
+
+
+class ControlSuiteEnv():
+    """Borrowed from https://github.com/Kaixhin/PlaNet/blob/master/env.py"""
+    def __init__(
+        self,
+        env: str,
+        seed: int, 
+        symbolic_env: bool,
+        max_episode_length: int, 
+        action_repeat: int, 
+        bit_depth: int
+        ):
+        from dm_control import suite
+        from dm_control.suite.wrappers import pixels
+        domain, task = env.split('-')
+        self.symbolic = symbolic_env
+        self._env = suite.load(domain_name=domain, task_name=task, task_kwargs={'random': seed})
+        if not symbolic_env:
+            self._env = pixels.Wrapper(self._env)
+        self.max_episode_length = max_episode_length
+        self.action_repeat = action_repeat
+        self.bit_depth = bit_depth
+
+    def reset(self):
+        self.t = 0  # Reset internal timer
+        state = self._env.reset()
+        if self.symbolic:
+            return torch.tensor(np.concatenate([np.asarray([obs]) if isinstance(obs, float) else obs for obs in state.observation.values()], axis=0), dtype=torch.float32).unsqueeze(dim=0)
+        else:
+            return images_to_observation(self._env.physics.render(camera_id=0), self.bit_depth)
+
+    def step(self, action):
+        action = action.detach().numpy()
+        reward = 0
+        for k in range(self.action_repeat):
+            state = self._env.step(action)
+            reward += state.reward
+            self.t += 1  # Increment internal timer
+            done = state.last() or self.t == self.max_episode_length
+            if done:
+                break
+        if self.symbolic:
+            observation = torch.tensor(np.concatenate([np.asarray([obs]) if isinstance(obs, float) else obs for obs in state.observation.values()], axis=0), dtype=torch.float32).unsqueeze(dim=0)
+        else:
+            observation = images_to_observation(self._env.physics.render(camera_id=0), self.bit_depth)
+        return observation, reward, done
+
+    def render(self):
+        cv2.imshow('screen', self._env.physics.render(camera_id=0)[:, :, ::-1])
+        cv2.waitKey(1)
+
+    def close(self):
+        cv2.destroyAllWindows()
+        self._env.close()
+
+    @property
+    def observation_size(self):
+        return sum([(1 if len(obs.shape) == 0 else obs.shape[0]) for obs in self._env.observation_spec().values()]) if self.symbolic else (3, 64, 64)
+
+    @property
+    def action_size(self):
+        return self._env.action_spec().shape[0]
+
+    @property
+    def action_range(self):
+        return float(self._env.action_spec().minimum[0]), float(self._env.action_spec().maximum[0]) 
+
+    # Sample an action randomly from a uniform distribution over all valid actions
+    def sample_random_action(self):
+        spec = self._env.action_spec()
+        return torch.from_numpy(np.random.uniform(spec.minimum, spec.maximum, spec.shape))
